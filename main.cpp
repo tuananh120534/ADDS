74c6dcac9b3231c165ad056d672b0d403aa8c19e
tuananh120534 <ndtanh.2005@gmail.com>
2025-03-25 16:45:06 +1030

    prac2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
-int main() {
+#include "Human.h"
+#include "Computer.h"
+#include "Referee.h"
+#include "Player.h"
+using namespace std;
+#include<iostream>
+int main(){
+    Human human("anh");
+    Computer comp;
+    cout<<comp.getName();
+    cout<<human.getName();
+    Referee ref;
+    Player* winner = ref.refGame(&human,&comp);
+    if (winner == nullptr) {
+        cout << "It's a Tie." << endl;
+    } else {
+        cout << winner->getName() << " Wins." << endl;
+    }
     return 0;
 }
